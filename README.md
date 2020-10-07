a89df25949ee94b70831467979d1a2097f7b8c54
socratone <gim2origin@gmail.com>
2020-10-08 00:23:23 +0900

    README.md 추가

---
+++ README.md
@@ -0,0 +1,3 @@
+아래 주소에 들어가서 babel.config.json 파일의 @babel/preset-env 옵션 리스트를 볼 수 있다.
+
+[https://babeljs.io/docs/en/babel-preset-env#targets]
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-const a = 'babel'
+const a = 'babel';
+
+function get() {
+  return new Promise((resolve, reject) => {
+    resolve('data');
+  });
+}
+
+get().then((data) => console.log('data:', data));
